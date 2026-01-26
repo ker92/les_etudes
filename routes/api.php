3a1fee09f8be8b2fa476a6dc6db89b276c34ca7c
= <jean.nguema@mewo-campus.fr>
2026-01-27 00:08:11 +0100

    Configuration des routes API CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FiliereController;
+use App\Http\Controllers\Api\DiplomeController;
+use App\Http\Controllers\Api\EtudiantController;
+use App\Http\Controllers\Api\AnneeController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::apiResource('filieres', FiliereController::class);
+Route::apiResource('diplomes', DiplomeController::class);
+Route::apiResource('annees', AnneeController::class);
+Route::apiResource('etudiants', EtudiantController::class);
